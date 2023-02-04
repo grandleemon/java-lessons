3aceefefe8711df37d34a80432cdf8d93078d36a
Max <zeetreex@gmail.com>
2023-02-04 21:58:12 +0200

    lesson2:variables

---
+++ Lesson2/Variables.java
@@ -0,0 +1,17 @@
+package Lesson2;
+
+public class Variables {
+    public static void main(String[] args) {
+        int intNumber = 10;
+        short shortNumber = 32767;
+        long longNumber = 341231231;
+
+        double doubleNumber = 240.21;
+        float floatNumber = 213.1f;
+
+        char character = 'q';
+        boolean bool = false;
+
+        byte byteNumber = 127;
+    }
+}
