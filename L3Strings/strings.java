588374004edab225fab694f9b1772466f04e2890
Max <zeetreex@gmail.com>
2023-02-08 21:02:54 +0200

    strings

---
+++ L3Strings/strings.java
@@ -0,0 +1,12 @@
+package L3Strings;
+
+class Strings {
+	public static void main(String[] args) {
+		int x = 1;
+		String s = "Hello";
+		String str = "Name";
+		System.out.println(s + str);
+		System.out.println("Hello " + "Name");
+		System.out.println("Number " + x);
+	}
+}
