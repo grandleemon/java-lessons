1ae7c10adddffdefe7a67f09c525493f86418946
Max <zeetreex@gmail.com>
2023-02-08 21:11:03 +0200

    while loop

---
+++ L4WhileLoop/WhileLoop.java
@@ -0,0 +1,11 @@
+package L4WhileLoop;
+
+class WhileLoop {
+	public static void main(String[] args) {
+		int value = 5;
+		while(value != 0) {
+			System.out.println(value);
+			value--;
+		}
+	}
+}
