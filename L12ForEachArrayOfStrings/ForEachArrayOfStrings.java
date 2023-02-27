c7df9f227187dc8e8ac6323ea2fda429313f31e1
Max <zeetreex@gmail.com>
2023-02-27 21:55:15 +0200

    L12ForEachArrayOfStrings

---
+++ L12ForEachArrayOfStrings/ForEachArrayOfStrings.java
@@ -0,0 +1,11 @@
+package L12ForEachArrayOfStrings;
+
+public class ForEachArrayOfStrings {
+  public static void main(String[] args) {
+    String[] strings = new String[3];
+
+    for(String string : strings) {
+      System.out.println(string);
+    }
+  }
+}
