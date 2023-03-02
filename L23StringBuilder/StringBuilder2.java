a440d8a9fe1f2ca27f705ad5bf981494cb4af013
Max <zeetreex@gmail.com>
2023-03-02 15:07:37 +0200

    L23StringBuilder

---
+++ L23StringBuilder/StringBuilder2.java
@@ -0,0 +1,9 @@
+package L23StringBuilder;
+
+public class StringBuilder2 {
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder("Hello");
+    sb.append(", ").append("world");
+    System.out.println(sb);
+  }
+}
