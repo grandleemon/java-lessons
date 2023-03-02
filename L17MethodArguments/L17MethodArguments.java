5e93da37296f3eeab2bd13a6009eadec7f425eae
Max <zeetreex@gmail.com>
2023-03-02 13:01:34 +0200

    L18SettersAndGetters

--- L17MethodArguments/L17MethodArguments.java
+++ L17MethodArguments/L17MethodArguments.java
@@ -3,19 +3,38 @@ package L17MethodArguments;
 public class L17MethodArguments {
   public static void main(String[] args) {
     Person person1 = new Person();
-    person1.age = 20;
+    person1.setAge(20);
     person1.setName("test");
-    int year = person1.calculateYearsToRetirement();
-    System.out.println(year);
+    person1.speak();
   }
 }
 
 class Person {
-  String name;
-  int age;
+  private String name;
+  private int age;
 
-  void setName(String name) {
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    if(name.isEmpty()) {
+      System.out.println("Empty");
+      return;
+    }
     this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    if(age < 0) {
+      System.out.println("Less than 0");
+      return;
+    }
+    this.age = age;
   }
 
   int calculateYearsToRetirement() {
