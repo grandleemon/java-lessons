f7e20183cfeaf16c41c8075256b855486d973c99
Max <zeetreex@gmail.com>
2023-03-02 15:16:03 +0200

    L23StringFormatting

--- L23StringBuilder/StringBuilder2.java
+++ L23StringBuilder/StringBuilder2.java
@@ -5,5 +5,10 @@ public class StringBuilder2 {
     StringBuilder sb = new StringBuilder("Hello");
     sb.append(", ").append("world");
     System.out.println(sb);
+
+    System.out.printf("Test %s \n", "eqw");
+    System.out.printf("Test %10d \n", 2222);
+    System.out.printf("Test %-10d \n", 2);
+    System.out.printf("Test %10.2f \n", 2.54444);
   }
 }
