467750bc9d3ccb0d7ca5a412bb44757060dff0ef
Max <zeetreex@gmail.com>
2023-03-02 13:48:23 +0200

    L21Static

--- L20Constructors/Constructors.java
+++ L20Constructors/Constructors.java
@@ -2,17 +2,25 @@ package L20Constructors;
 
 public class Constructors {
   public static void main(String[] args) {
-    Human human1 = new Human("test", 20);
+    Human.description = "test";
+    Human human1 = new Human("bob", 33);
+    human1.printNumberOfPeople();
+    Human human2 = new Human("bob", 33);
+    human2.printNumberOfPeople();
+    human1.getAllFields();
   }
 }
 
 class Human {
   private String name;
   private int age;
+  public static String description;
+  public static int countPeople;
 
   Human(String name, int age) {
     this.name = name;
     this.age = age;
+    countPeople++;
   }
 
   public void setName(String name) {
@@ -21,5 +29,12 @@ class Human {
 
   public void setAge(int age) {
     this.age = age;
+  }
+
+  public void getAllFields() {
+    System.out.println(name + age + description);
+  }
+  public void printNumberOfPeople() {
+    System.out.println(countPeople);
   }
 }
