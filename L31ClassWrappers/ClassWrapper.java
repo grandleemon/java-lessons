f13a36e1937afd445fea32ef71f87d90b68e99b5
Max <zeetreex@gmail.com>
2023-03-03 10:35:37 +0200

    L31ClassWrappers

---
+++ L31ClassWrappers/ClassWrapper.java
@@ -0,0 +1,11 @@
+package L31ClassWrappers;
+
+public class ClassWrapper {
+  public static void main(String[] args) {
+    int x = 1;
+    Integer x2 = 1;
+    int y = x2;
+
+    Integer.parseInt("123");
+  }
+}
