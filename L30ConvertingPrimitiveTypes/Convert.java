a2badf834e82f579c7eb8c37baec49eae630ea3a
Max <zeetreex@gmail.com>
2023-03-03 10:29:46 +0200

    L30ConvertingPrimitiveTypes

---
+++ L30ConvertingPrimitiveTypes/Convert.java
@@ -0,0 +1,13 @@
+package L30ConvertingPrimitiveTypes;
+
+public class Convert {
+  public static void main(String[] args) {
+    float f = 123.2f;
+    long l = 22323L;
+
+    int x = (int)l;
+
+    int a = 123;
+    double d = a;
+  }
+}
